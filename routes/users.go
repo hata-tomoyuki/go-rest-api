fce281e8c434e663b904c037478be26a1a085f69
hata-tomoyuki <h.tomo0602@gmail.com>
2026-03-22 17:32:54 +0900

    user Login

--- routes/users.go
+++ routes/users.go
@@ -26,3 +26,23 @@ func signup(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully", "user_id": user.ID})
 }
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+}
