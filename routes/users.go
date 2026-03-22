cc8b5ef9aed57759e32d53eaa7494bf8aad59c15
hata-tomoyuki <h.tomo0602@gmail.com>
2026-03-22 17:09:52 +0900

    CREATE user

---
+++ routes/users.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	err = user.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully", "user_id": user.ID})
+}
