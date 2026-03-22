1ee42a41813dd7c3ae78ff99f4c395fc65e6abcd
hata-tomoyuki <h.tomo0602@gmail.com>
2026-03-22 16:28:55 +0900

    mod func name

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
+
 }
