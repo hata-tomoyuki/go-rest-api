00e72179b1006e98e13e4777f88496b6db779ec8
hata-tomoyuki <h.tomo0602@gmail.com>
2026-03-23 13:29:53 +0900

    cancel event

--- routes/register.go
+++ routes/register.go
@@ -32,3 +32,24 @@ func registerForEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Successfully registered for event"})
 }
+
+func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt((context.Param("id")), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Could not parse event ID"})
+		return
+	}
+
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Successfully cancelled registration for event"})
+}
